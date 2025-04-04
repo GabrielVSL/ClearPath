f41f73b374006846a8758e048b20aa1830a44f5c
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2025-04-04 13:51:33 -0300

    Initial commit

---
+++ src/ClearPath/ClearPath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ClearPath
+//
+//  Created by Turma01-Backup on 04/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ src/ClearPath/ClearPath/DicasView.swift
@@ -0,0 +1,25 @@
+//
+//  DicasView.swift
+//  ClearPath
+//
+//  Created by Turma01-Backup on 04/04/25.
+//
+
+import SwiftUI
+
+struct DicasView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                
+            }
+            .frame(width: 370, height: 116)
+            .padding()
+        }
+    }
+    
+}
+
+#Preview {
+    DicasView()
+}
