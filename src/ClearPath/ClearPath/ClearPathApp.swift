ab6a28f1baf54597d8eb82500607e54b609783d5
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2025-04-04 14:15:28 -0300

    Initial commit

--- src/ClearPath/ClearPath/ClearPathApp.swift
+++ src/ClearPath/ClearPath/ClearPathApp.swift
@@ -1,1 +1,18 @@
+//
+//  ClearPathApp.swift
+//  ClearPath
+//
+//  Created by Turma01-Backup on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ClearPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
 
